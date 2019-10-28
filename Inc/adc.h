b8636c10ba66dc5d8c470f166be1134f12123522
dots111 <alter001@163.com>
2019-10-28 16:55:55 +0800

    init

---
+++ Inc/adc.h
@@ -0,0 +1,10 @@
+#if !defined(__ADC_H)
+#define __ADC_H
+#include "stm32f0xx_ll_adc.h"
+#include "stm32f0xx_ll_dma.h"
+
+#define ADBufferSize 8
+extern uint16_t ADC_ConvertedValue[ADBufferSize];
+extern void start_adc_dma(ADC_TypeDef *adc, DMA_TypeDef *dma);
+
+#endif // __ADC_H
---
+++ Src/adc.c
@@ -0,0 +1,23 @@
+#include "adc.h"
+
+uint16_t ADC_ConvertedValue[ADBufferSize];
+
+void start_adc_dma(ADC_TypeDef *adc, DMA_TypeDef *dma)
+{
+    LL_DMA_SetDataLength(DMA1, LL_DMA_CHANNEL_1, ADBufferSize);
+
+    LL_DMA_SetPeriphAddress(DMA1, LL_DMA_CHANNEL_1, LL_ADC_DMA_GetRegAddr(adc, LL_ADC_DMA_REG_REGULAR_DATA));
+
+    LL_DMA_SetMemoryAddress(DMA1, LL_DMA_CHANNEL_1, (uint32_t)ADC_ConvertedValue);
+
+    LL_DMA_EnableChannel(DMA1, LL_DMA_CHANNEL_1);
+
+    LL_ADC_StartCalibration(adc);
+
+    while (LL_ADC_IsCalibrationOnGoing(adc))
+        ;
+
+    LL_ADC_Enable(adc);
+
+    LL_ADC_REG_StartConversion(adc);
+}
